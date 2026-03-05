de083f10faf92fc0f9ddb9329f42e3c31725315a
[fhyath] <fhyath2@student.gsu.edu>
2026-03-04 23:37:01 -0500

    Make Home page look better

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,57 +1,101 @@
 import 'package:flutter/material.dart';
-import '../data/recipes_data.dart';
 import '../models/recipe.dart';
+import '../data/recipes_data.dart';
 import 'details_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  void openRecipe(BuildContext context, Recipe recipe) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (_) => DetailsScreen(recipe: recipe),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Recipe Book"),
+        title: const Text(
+          "My Recipe Book",
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.orange,
       ),
 
-      body: ListView.builder(
-        itemCount: sampleRecipes.length,
+      body: Container(
+        color: Colors.orange.shade50,
 
-        itemBuilder: (context, index) {
-          final recipe = sampleRecipes[index];
+        child: Column(
+          children: [
 
-          return Card(
-            margin: const EdgeInsets.symmetric(
-              horizontal: 12,
-              vertical: 6,
-            ),
-
-            child: ListTile(
-              leading: Image.asset(
-                recipe.imagePath,
-                width: 56,
-                height: 56,
-                fit: BoxFit.cover,
+            const Padding(
+              padding: EdgeInsets.all(16),
+              child: Text(
+                "🍽️ Today's Recipes",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-
-              title: Text(recipe.name),
-
-              trailing: const Icon(Icons.chevron_right),
-
-              onTap: () {
-                openRecipe(context, recipe);
-              },
             ),
-          );
-        },
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: sampleRecipes.length,
+
+                itemBuilder: (context, index) {
+
+                  final recipe = sampleRecipes[index];
+
+                  return Card(
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 10),
+
+                    elevation: 4,
+
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    child: ListTile(
+                      contentPadding: const EdgeInsets.all(10),
+
+                      leading: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset(
+                          recipe.imagePath,
+                          width: 70,
+                          height: 70,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+
+                      title: Text(
+                        recipe.name,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      trailing: const Icon(Icons.restaurant_menu),
+
+                      onTap: () {
+                        Navigator.push(
+                          context,
+
+                          MaterialPageRoute(
+                            builder: (_) =>
+                                DetailsScreen(recipe: recipe),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
